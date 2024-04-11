59e1b87944e93b98eccca4ae01b44ea302cb5040
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-04-11 14:32:18 +0800

    增加翻译接口

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ app.get('/painter/generate', painter_generate)
 // 浏览器新标签页: 和风天气
 import { weather } from './routes/weather.js'
 app.get('/weather', weather)
+// 赛博画师小叶子: 中译英
+import { painter_translate } from './routes/painter_translate.js'
+app.post('/painter/translate', painter_translate)
 
 // 404
 app.all('*', () => new Response('请求路径错误 / Not Found', { status: 404 }))
---
+++ src/routes/painter_translate.js
@@ -0,0 +1,30 @@
+export async function painter_translate(c) {
+  try {
+    // 请求参数
+    const url = `https://api.cloudflare.com/client/v4/accounts/${c.env.CF_USER}/ai/run/@cf/meta/m2m100-1.2b`
+    const body = await c.req.json()
+    // 发送请求
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${c.env.CF_API_KEY}`
+      },
+      body: JSON.stringify(body)
+    })
+    const result = await response.json()
+    // 返回结果
+    if (result.success) {
+      return new Response(JSON.stringify(result), {
+        headers: {
+          'content-type': 'application/json',
+          'Access-Control-Allow-Origin': `${c.env.CLIENT}`,
+        }
+      })
+    } else {
+      return new Response('翻译失败', { status: 500 })
+    }
+  } catch(err) {
+    return new Response(err.message, { status: 500 })
+  }
+}
