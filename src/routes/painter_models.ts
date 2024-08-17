8625c1bff08a07b29e3503dfbda11a72447be583
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-08-17 18:18:41 +0800

    up

--- src/routes/painter_models.ts
+++ src/routes/painter_models.ts
@@ -13,10 +13,8 @@ const MODELS: Models = {
   '@cf/stabilityai/stable-diffusion-xl-base-1.0': { description: '☁️ SDXL Base 1.0', type: 'textToImage', lang: 'natural' },
   '@cf/bytedance/stable-diffusion-xl-lightning': { description: '☁️ SDXL Lightning', type: 'textToImage', lang: 'natural' },
   '@hf/cagliostrolab/animagine-xl-3.1': { description: '🤗 AnimagineXL 3.1', type: 'textToImage', lang: 'sdPrompt' },
-  '@hf/Koolchh/AnimeBoysXL-v3.0': { description: '🤗 AnimeBoysXL 3.0', type: 'textToImage', lang: 'sdPrompt' },
   '@hf/UnfilteredAI/NSFW-GEN-ANIME-v2': { description: '🤗 NSFWGenAnime', type: 'textToImage', lang: 'sdPrompt' },
-  '@hf/ehristoforu/dalle-3-xl-v2': { description: '🤗 SDXL DALL-E', type: 'textToImage', lang: 'natural' },
-  // '@cf/stabilityai/stable-diffusion-xl-turbo': { description: '☁️ SDXL Turbo', type: 'both', lang: 'natural' },
+  '@hf/black-forest-labs/FLUX.1-schnell': { description: '🤗 FLUX.1 Schnell', type: 'textToImage', lang: 'natural' },
   '@cf/runwayml/stable-diffusion-v1-5-img2img': { description: '☁️ StableDiffusion 1.5 Img2Img', type: 'imageToImage', lang: 'sdPrompt' },
 }
 
