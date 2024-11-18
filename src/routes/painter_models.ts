101069e8964a6e92a7ee04c0c6b61d42a65cdc6b
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-11-18 10:36:39 +0800

    update model

--- src/routes/painter_models.ts
+++ src/routes/painter_models.ts
@@ -14,10 +14,11 @@ const MODELS: Models = {
   '@cf/black-forest-labs/flux-1-schnell': { description: '☁️ FLUX.1 Schnell', type: 'textToImage', lang: 'natural' },
   '@cf/stabilityai/stable-diffusion-xl-base-1.0': { description: '☁️ SDXL Base 1.0', type: 'textToImage', lang: 'natural' },
   '@cf/bytedance/stable-diffusion-xl-lightning': { description: '☁️ SDXL Lightning', type: 'textToImage', lang: 'natural' },
-  '@hf/cagliostrolab/animagine-xl-3.1': { description: '🤗 AnimagineXL 3.1', type: 'textToImage', lang: 'sdPrompt' },
-  '@hf/UnfilteredAI/NSFW-GEN-ANIME-v2': { description: '🤗 NSFWGenAnime', type: 'textToImage', lang: 'sdPrompt' },
   '@hf/black-forest-labs/FLUX.1-schnell': { description: '🤗 FLUX.1 Schnell', type: 'textToImage', lang: 'natural' },
   '@hf/black-forest-labs/FLUX.1-dev': { description: '🤗 FLUX.1 Dev', type: 'textToImage', lang: 'natural' },
+  '@hf/stabilityai/stable-diffusion-3.5-large': { description: '🤗 SD 3.5 Large', type: 'textToImage', lang: 'natural' },
+  '@hf/cagliostrolab/animagine-xl-3.1': { description: '🤗 AnimagineXL 3.1', type: 'textToImage', lang: 'sdPrompt' },
+  '@hf/UnfilteredAI/NSFW-GEN-ANIME-v2': { description: '🤗 NSFWGenAnime', type: 'textToImage', lang: 'sdPrompt' },
   '@cf/runwayml/stable-diffusion-v1-5-img2img': { description: '☁️ StableDiffusion 1.5 Img2Img', type: 'imageToImage', lang: 'sdPrompt' },
 }
 
